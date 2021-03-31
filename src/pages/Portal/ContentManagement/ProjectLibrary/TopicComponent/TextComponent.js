d677c46a8861cc8fa0f8baaf101ae8a43ce794fc
yanghao <aa63851976@qq.com>
2021-03-31 15:22:40 +0800

    新增文本

--- src/pages/Portal/ContentManagement/ProjectLibrary/TopicComponent/TextComponent.js
+++ src/pages/Portal/ContentManagement/ProjectLibrary/TopicComponent/TextComponent.js
@@ -2,14 +2,14 @@
  * @Author: zqm 
  * @Date: 2021-02-17 17:03:48 
  * @Last Modified by: mikey.zhaopeng
- * @Last Modified time: 2021-03-31 10:18:34
+ * @Last Modified time: 2021-03-31 15:20:51
  * 创建工地
  */
 import React, { PureComponent } from 'react';
 import ContentEditable from 'react-contenteditable';
 import { connect } from 'dva';
-import Upload from '@/components/Upload/Upload';
-import { Icon } from 'antd';
+import { Icon, Drawer, Input } from 'antd';
+import { SketchPicker } from 'react-color';
 import styles from './index.less';
 @connect(({ ProjectLibrary }) => ({
   ProjectLibrary,
@@ -18,13 +18,13 @@ class TextComponent extends PureComponent {
   constructor() {
     super();
     this.contentEditable = React.createRef();
-    this.state = { isTrue: 1 };
+    this.state = { isTrue: 1, visible: false, show: false };
   }
   componentDidMount() {}
 
   render() {
     const { data, index } = this.props;
-    const { isTrue } = this.state;
+    const { isTrue, visible, show } = this.state;
     return (
       <div
         className={data.checked === 1 ? styles.ViewFormsg : styles.ViewFormborders}
@@ -48,14 +48,16 @@ class TextComponent extends PureComponent {
         <div className={data.checked === 1 ? styles.roundRightTop : ''} />
         <div className={data.checked === 1 ? styles.roundLeftBottom : ''} />
         <div className={data.checked === 1 ? styles.roundRightBottom : ''} />
-        <ContentEditable
-          innerRef={this.contentEditable}
-          html={data.paramList[0].defaultValue} // innerHTML of the editable div
-          disabled={false} // use true to disable editing
-          onChange={this.handleChange} // handle innerHTML change
-          onBlur={this.handleonBlur}
-          tagName="article" // Use a custom HTML tag (uses a div by default)
-        />
+        <div className={styles.arwrap}>
+          <ContentEditable
+            innerRef={this.contentEditable}
+            html={data.paramList[0].defaultValue} // innerHTML of the editable div
+            disabled={false} // use true to disable editing
+            onChange={this.handleChange} // handle innerHTML change
+            onBlur={this.handleonBlur}
+            tagName="article" // Use a custom HTML tag (uses a div by default)
+          />
+        </div>
         {data.checked === 1 ? (
           <span
             className={styles.closePic}
@@ -68,6 +70,46 @@ class TextComponent extends PureComponent {
         ) : (
           ''
         )}
+        <Drawer
+          width="550"
+          title="编辑文本"
+          placement="right"
+          onClose={this.onClose}
+          visible={visible}
+          mask={false}
+        >
+          <div>文本</div>
+          <div className="clearfix">
+            <div className={styles.fontC}>
+              <Icon type="font-size" />
+              <Input style={{ width: 80 }} onChange={e => this.handleListChange(e, 'fontSize')} />
+            </div>
+            <div className={styles.btnColorWrap}>
+              <div
+                className={styles.btnColor}
+                style={{ background: data.elementButtonColor }}
+                onClick={() => {
+                  this.showColor();
+                }}
+              />
+            </div>
+          </div>
+          {show ? (
+            <div className={styles.SketchWrap}>
+              <SketchPicker color={data.elementButtonColor} onChange={this.handleColorChange} />
+              <span
+                className={styles.closeColor}
+                onClick={() => {
+                  this.closeColor();
+                }}
+              >
+                <Icon type="close-circle" />
+              </span>
+            </div>
+          ) : (
+            ''
+          )}
+        </Drawer>
       </div>
     );
   }
@@ -75,6 +117,9 @@ class TextComponent extends PureComponent {
   changePic() {
     const { index } = this.props;
     this.props.handleCheck(index);
+    this.setState({
+      visible: true,
+    });
   }
   deletePic() {
     const { index } = this.props;
@@ -111,6 +156,26 @@ class TextComponent extends PureComponent {
       isTrue: 1,
     });
   };
+  onClose = () => {
+    this.setState({
+      visible: false,
+    });
+  };
+  showColor() {
+    this.setState({
+      show: true,
+    });
+  }
+  handleColorChange = value => {
+    const { index } = this.props;
+    this.props.handleColor(value.hex, index, 1);
+  };
+  closeColor() {
+    this.setState({
+      show: false,
+    });
+  }
+  handleListChange(name, index) {}
 }
 
 export default TextComponent;
