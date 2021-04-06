284f9441bfca451e1c9bc9622c2b8e4caa7246a1
DESKTOP-1CQD9MH\zqm <zhouqingmeng@in-deco.com>
2021-04-06 14:08:07 +0800

    首页更新

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,9 +1,11 @@
 import React, { PureComponent } from 'react';
+import welcome from '../assets/img_welcome@2x.png';
 class Welcome extends PureComponent {
   render() {
     return (
-      <div ref="root">
-        <p style={{fontWeight:600,fontSize:40,textAlign:'center'}}>营销站</p>
+      <div style={{textAlign:'center',paddingTop:160}}>
+        <img src={welcome} alt="营销站" style={{ width: 284, height: 224 ,marginBottom:70}} />
+        <p style={{fontWeight:400,fontSize:20,textAlign:'center',color:"#222"}}>欢迎使用<span style={{color:'#fe6a30'}}>营销站</span>管理后台</p>
       </div>
     );
   }
