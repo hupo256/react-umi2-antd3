0d4549a09d68217cab9204b5071160f5f3bf70fa
yinguojie <yinguojie@in-deco.com>
2021-04-27 16:47:37 +0800

    修改自定义代码tab

--- src/pages/Portal/ContentManagement/WebSetting/CustomCode.js
+++ src/pages/Portal/ContentManagement/WebSetting/CustomCode.js
@@ -52,46 +52,23 @@ class CustomCode extends Component {
     return (
       <div>
         <div style={{ color: '#101010', fontSize: '26px', marginBottom: '20px' }}>自定义代码</div>
-        <Tabs className="cystomCodeTabs">
-          <TabPane tab="页眉" key="1">
-            <TextArea
-              type="text"
-              max={5000}
-              value={headerValue}
-              style={{ width: 884, height: 650 }}
-              onChange={e => this.changeHeaderValue(e)}
-              placeholder="一般用于植入各种社交分享、在线咨询（QQ、百度商桥、美洽...）、网站统计（CNZZ）等小插件的代码！！！HTML将被插入标记对之间。在此处，你可以添加元标记并加入脚本。请务必对CSS使用标记对，对JavaScript使用标记对！【同一区域仅支持一种组件】"
-            />
-            <div>
-              <Button
-                className="defaultHostButton"
-                style={{ marginTop: '20px' }}
-                onClick={this.onSave.bind(this)}
-              >
-                保存
-              </Button>
-            </div>
-          </TabPane>
-          <TabPane tab="页尾" key="2">
-            <TextArea
-              type="text"
-              max={5000}
-              value={footerValue}
-              style={{ width: 884, height: 650 }}
-              onChange={e => this.changeFooterValue(e)}
-              placeholder="一般用于植入各种社交分享、在线咨询（QQ、百度商桥、美洽...）、网站统计（CNZZ）等小插件的代码！！！HTML将被插入标记对之间。在此处，你可以添加元标记并加入脚本。请务必对CSS使用标记对，对JavaScript使用标记对！【同一区域仅支持一种组件】"
-            />
-            <div>
-              <Button
-                className="defaultHostButton"
-                style={{ marginTop: '20px' }}
-                onClick={this.onSave.bind(this)}
-              >
-                保存
-              </Button>
-            </div>
-          </TabPane>
-        </Tabs>
+        <TextArea
+          type="text"
+          max={5000}
+          value={headerValue}
+          style={{ width: 884, height: 650 }}
+          onChange={e => this.changeHeaderValue(e)}
+          placeholder="一般用于植入各种社交分享、在线咨询（QQ、百度商桥、美洽...）、网站统计（CNZZ）等小插件的代码！！！HTML将被插入标记对之间。在此处，你可以添加元标记并加入脚本。请务必对CSS使用标记对，对JavaScript使用标记对！【同一区域仅支持一种组件】"
+        />
+        <div>
+          <Button
+            className="defaultHostButton"
+            style={{ marginTop: '20px' }}
+            onClick={this.onSave.bind(this)}
+          >
+            保存
+          </Button>
+        </div>
       </div>
     );
   }
