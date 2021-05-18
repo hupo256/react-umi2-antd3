14f22bb680ce8a36ff3ccda081b1accfc67e98fe
ximing <ximing@in-deco.com>
2021-05-18 16:57:37 +0800

    后台返回万元

--- src/pages/PcPreview/Case/Case.jsx
+++ src/pages/PcPreview/Case/Case.jsx
@@ -12,9 +12,9 @@ const CaseProjects = ({ data, domain = '' }) => {
   const caseStyle = {}
   _.forEach(data, (item, index) => {
     data[index]['name'] = `${item.title.length > 6 ? item.title.slice(0, 6) + '...' : item.title}`
-    data[index]['text'] = `${item.acreage}m² | ${CHN_NUM_CHAR[item.bedroom]}室${
-      CHN_NUM_CHAR[item.liveroom]
-    }厅 | ${item.decorationCost / 10000}万元`
+    data[index]['text'] = `${item.acreage}m² | ${CHN_NUM_CHAR[item.bedroom]}室${CHN_NUM_CHAR[item.liveroom]}厅 | ${
+      item.decorationCost
+    }万元`
     caseStyle[`image${index}`] = {
       background: `url(${item.coverPicUrl}) no-repeat center center`,
       backgroundSize: 'cover',
