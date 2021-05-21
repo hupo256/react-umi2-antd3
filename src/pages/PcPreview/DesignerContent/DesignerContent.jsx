f8cf088cfcbe1dc3fe1e74345b728755662345c1
ximing <ximing@in-deco.com>
2021-05-21 16:23:39 +0800

    fix SITE-2858

--- src/pages/PcPreview/DesignerContent/DesignerContent.jsx
+++ src/pages/PcPreview/DesignerContent/DesignerContent.jsx
@@ -50,15 +50,17 @@ const DesignerContent = ({ data, domain }) => {
                       </div>
                       <div className={styles.right}>
                         <h3 className={styles.titleWrap}>
-                          <p className={styles.name}>{value.name}</p>
-                          <div className={styles.jobTitle}>{value.position}</div>
+                          <p className={styles.name}>
+                            {value.name.length > 4 ? `${value.name.slice(0, 4)}...` : value.name}
+                          </p>
+                          <div className={styles.jobTitle}>
+                            {value.position.length > 6 ? `${value.position.slice(0, 6)}...` : value.position}
+                          </div>
                         </h3>
                         <p className={styles.content}>
-                          {value.designConcept
-                            ? value.designConcept.length > 36
-                              ? `${value.designConcept.slice(0, 36)}...`
-                              : value.designConcept
-                            : null}
+                          {value.designConcept.length > 43
+                            ? `${value.designConcept.slice(0, 43)}...`
+                            : value.designConcept}
                         </p>
                       </div>
                     </div>
