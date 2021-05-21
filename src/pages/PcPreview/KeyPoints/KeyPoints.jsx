b23a2fc1f905d281d1f0492e9bbe8c2f72228e13
ximing <ximing@in-deco.com>
2021-05-21 11:58:15 +0800

    使 keyFeature 限制一直

--- src/pages/PcPreview/KeyPoints/KeyPoints.jsx
+++ src/pages/PcPreview/KeyPoints/KeyPoints.jsx
@@ -2,30 +2,32 @@ import styles from './KeyPoints.less'
 import _ from 'lodash'
 
 import { typeMap, paramMap } from '../constants.js'
+
+const isHide = feature => {
+  return feature.type === 'games' || !feature.uid || !feature.type
+}
+
 const KeyPoints = ({ pointsList, domain }) => {
   return (
     <div className={styles.featurePoints}>
-      {_.map(
-        pointsList,
-        (feature, index) =>
-          feature.type === 'games' || (
-            <div
-              key={index}
-              className={styles.featurePoint}
-              onClick={() =>
-                window.open(
-                  `${domain}/${typeMap[feature.type]}/details?${paramMap[feature.type]}=${feature.uid}`,
-                  '页面预览',
-                )
-              }
-              style={{ cursor: 'pointer' }}
-            >
-              <img src={feature.icon} />
-              <p className={styles.pointTitle}>{feature.title}</p>
-              <p className={styles.pointSubTitle}>{feature.desc}</p>
-            </div>
-          ),
-      )}
+      {_.map(pointsList, (feature, index) => (
+        <div
+          key={index}
+          className={styles.featurePoint}
+          onClick={() =>
+            isHide(feature) ||
+            window.open(
+              `${domain}/${typeMap[feature.type]}/details?${paramMap[feature.type]}=${feature.uid}`,
+              '页面预览',
+            )
+          }
+          style={{ cursor: isHide(feature) ? 'default' : 'pointer' }}
+        >
+          <img src={feature.icon} />
+          <p className={styles.pointTitle}>{feature.title}</p>
+          <p className={styles.pointSubTitle}>{feature.desc}</p>
+        </div>
+      ))}
     </div>
   )
 }
