7908b72ba5f2a1b5d732771e58baa9b335d7e6fb
ximing <ximing@in-deco.com>
2021-05-24 15:07:02 +0800

    remove alt

--- src/pages/PcPreview/HomePage.jsx
+++ src/pages/PcPreview/HomePage.jsx
@@ -140,7 +140,6 @@ const Home = () => {
               <div className={styles.companyHeaderStyle}>
                 <img
                   src={footerData.logo}
-                  alt={footerData.companyName}
                   className={styles.logoStyle}
                   onClick={() => window.open('/', '页面预览')}
                   style={{ cursor: 'pointer' }}
