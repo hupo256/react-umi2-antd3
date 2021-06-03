bd4868a0dacdc9cca53723c1c75d0b4f30eabe5f
ximing <ximing@in-deco.com>
2021-06-03 14:36:04 +0800

    update contants.js

--- src/pages/PcPreview/constants.js
+++ src/pages/PcPreview/constants.js
@@ -9,4 +9,12 @@ export const typeMap = {
 
 export const paramMap = { case: 'uid', site: 'gongdiUid', article: 'articleUid', design: 'uid', special: 'uid' }
 
-export const domain = 'http://dev-site.ingongdi.com'
+// 来源：TSC008-首页，TSC009-案例，TSC010-工地，TSC011-设计师，TSC012-文章，TSC013-专题
+export const pushMsgMap = {
+  home: 'TSC008',
+  case: 'TSC009',
+  site: 'TSC010',
+  designer: 'TSC011',
+  article: 'TSC012',
+  special: 'TSC013',
+}
