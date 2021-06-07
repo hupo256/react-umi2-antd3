d9b979fb833902dff07c3b2fac64f8f0131456b1
屈乐 <qule@in-deco.com>
2021-06-07 13:47:54 +0800

    增加小游戏频道选项

--- src/services/channelManage.js
+++ src/services/channelManage.js
@@ -1,7 +1,7 @@
 /*
  * @Author: ql
  * @Date: 2021-04-22 10:55:31
- * @LastEditTime: 2021-05-19 11:13:21
+ * @LastEditTime: 2021-06-04 18:45:04
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \front-wechat-saas\src\services\channelManage.js
@@ -82,6 +82,12 @@ export const articleDicApi = body => request(`${baseurl}/api/v1/wechat/dic/query
 
 // 专题列表接口
 export const specialListApi = body => request(`${baseurl}/api/v1/wechat/special/pageList`, {
+    method: 'POST',
+    body
+})
+
+// 小游戏列表接口
+export const activeListApi = body => request(`${baseurl}/api/v1/wechat/activity/queryActivityList`, {
     method: 'POST',
     body
 })
