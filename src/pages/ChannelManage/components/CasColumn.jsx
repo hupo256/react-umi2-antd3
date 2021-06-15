d31f061e41b55b05c45125ac5de685bbab56aef8
hupo256 <hupo256@qq.com>
2021-06-15 14:37:33 +0800

    done cascadeSelect components

---
+++ src/pages/ChannelManage/components/CasColumn.jsx
@@ -0,0 +1,154 @@
+/*
+ * @Author: tdd 
+ * @Date: 2020-06-17 17:35:33 
+ * @Last Modified by: tdd
+ * @Last Modified time: 2021-06-17 18:09:47
+ * 级联选择器要用的的表头
+ */
+
+import React from 'react';
+import { Tooltip } from 'antd';
+import styles from '../index.less';
+
+const HeadTip = ({ text }) => (
+  <Tooltip placement="topLeft" title={text}>
+    <p className={styles.cellCon}>{text}</p>
+  </Tooltip>
+);
+
+export const ColumnsObj = {
+  // 工地详情页表头
+  columns_1: [
+    {
+      title: '工地',
+      key: 'gongdiTitle',
+      dataIndex: 'gongdiTitle',
+      render: (text, r) => <HeadTip text={text} />,
+    },
+    {
+      title: '工地信息',
+      key: 'buildingName',
+      dataIndex: 'buildingName',
+      render: (text, r) => <HeadTip text={text} />,
+    },
+    {
+      title: '阶段',
+      key: 'gongdiStageName',
+      dataIndex: 'gongdiStageName',
+    },
+  ],
+
+  // 设计师表头
+  columns_2: [
+    {
+      title: '设计师',
+      key: 'name',
+      dataIndex: 'name',
+      render: (text, record) => (
+        <div className={styles.desigTit}>
+          <img src={record?.headPicUrl} alt="" srcSet="" />
+          <span>{text}</span>
+        </div>
+      ),
+    },
+    {
+      title: '职级',
+      key: 'position',
+      dataIndex: 'position',
+    },
+    {
+      title: '案例数',
+      key: 'caseNum',
+      dataIndex: 'caseNum',
+    },
+  ],
+
+  // 案例表头
+  columns_3: [
+    {
+      title: '案例',
+      key: 'titleInfo',
+      dataIndex: 'title',
+      render: (text, r) => <HeadTip text={text} />,
+    },
+    {
+      title: '案例信息',
+      key: 'buildingName',
+      dataIndex: 'buildingName',
+      render: (text, r) => <HeadTip text={text} />,
+    },
+    {
+      title: '设计师',
+      key: 'designerName',
+      dataIndex: 'designerName',
+      render: (text, r) => <HeadTip text={text} />,
+    },
+  ],
+
+  // 文章表头
+  columns_4: [
+    {
+      title: '文章标题',
+      key: 'articleTitle',
+      dataIndex: 'articleTitle',
+      render: (text, r) => <HeadTip text={text} />,
+    },
+    {
+      title: '发布人',
+      key: 'creatorName',
+      dataIndex: 'creatorName',
+      render: (text, r) => <HeadTip text={text} />,
+    },
+    {
+      title: '更新时间',
+      key: 'updateTime',
+      dataIndex: 'updateTime',
+    },
+  ],
+  // 专题表头
+  columns_5: [
+    {
+      title: '专题标题',
+      key: 'specialTitle',
+      dataIndex: 'specialTitle',
+      render: (text, r) => <HeadTip text={text} />,
+    },
+    {
+      title: '创建人',
+      key: 'creatorName',
+      dataIndex: 'creatorName',
+      render: (text, r) => <HeadTip text={text} />,
+    },
+    {
+      title: '更新时间',
+      key: 'updateTime',
+      dataIndex: 'updateTime',
+    },
+  ],
+  // 小游戏表头
+  columns_6: [
+    {
+      title: '游戏标题',
+      key: 'activityTitle',
+      dataIndex: 'activityTitle',
+      render: (text, r) => <HeadTip text={text} />,
+    },
+    {
+      title: '状态',
+      key: 'status',
+      dataIndex: 'status',
+      render: (text, r) => {
+        let tex = '未开始';
+        text === 1 && (tex = '进行中');
+        text === 2 && (tex = '已结束');
+        return tex;
+      },
+    },
+    {
+      title: '创建人',
+      key: 'creater',
+      dataIndex: 'creater',
+      render: (text, r) => <HeadTip text={text} />,
+    },
+  ],
+};
---
+++ src/pages/ChannelManage/components/CascadeSelect.jsx
@@ -0,0 +1,224 @@
+/*
+ * @Author: tdd 
+ * @Date: 2020-06-17 14:35:33 
+ * @Last Modified by: tdd
+ * @Last Modified time: 2021-06-17 15:09:47
+ * 关联页面级联选择器
+ */
+
+import React, { useState, useEffect } from 'react'
+import { siteListApi, designerListApi, caseListApi, articleListApi, articleDicApi, specialListApi, activeListApi  } from '@/services/channelManage'
+import { Input, Tabs, Table, Radio } from 'antd'
+import { ColumnsObj } from './CasColumn'
+import styles from '../index.less'
+
+const { TabPane } = Tabs
+const { Search } = Input;
+export default function CascadeSelect(props){
+    const { optsArr, curNavs=[], callFun } = props
+
+    const [showSelectPanl, setshowSelectPanl] = useState(false)  // 选择器的显示开关
+    const [relatedPageOption, setrelatedPageOption] = useState([])  // 生成级联选择的数据
+    const [currentSelectRelatedPageOpt, setcurrentSelectRelatedPageOpt] = useState([])  // 当前已选
+    const [articleDicOpts, setarticleDicOpts] = useState([])  
+    const [currentarticleDicCode, setcurrentarticleDicCode] = useState(undefined)  
+    const [currentKey, setcurrentKey] = useState('0')  
+    const [detData, setdetData] = useState(null)  // 查询出来的详情列表
+    const [detailType, setdetailType] = useState(0)   // 祥情页类型 
+    const [pageNum, setpageNum] = useState(1)  
+
+    useEffect(() => {
+        showSelect()
+    }, [])
+
+    useEffect(() => {
+        detailType && getDataList();
+    }, [detailType])
+
+    function showSelect(){
+        setshowSelectPanl(true)
+        setcurrentSelectRelatedPageOpt([])
+        setrelatedPageOption(filterLevelOps())
+    }
+
+    // 格式化回显
+    function formatData(opts){
+        console.log(opts)
+        let arr = [];
+        opts.forEach((opt, ind) => {
+            const {name, title, articleTitle,gongdiTitle,specialTitle, activityTitle,
+                uid, gongdiUid, articleUid, specialUid, icon, appletsLink, linkKey, linkType } = opt;
+            const isEnd = (ind === 1 && linkType === 1) || ind === 2 // 顺便处理是否到末级的逻辑
+            arr.push({
+                text: name || title || articleTitle  || gongdiTitle || specialTitle || activityTitle,
+                code: uid || gongdiUid || articleUid || specialUid,
+                icon, linkKey, linkType, appletsLink,  isEnd
+            })
+        })
+        return arr;
+    }
+
+    // 解构重组后台数据 
+    function format(data){
+        if (!Array.isArray(data)) return;
+        let newArr = [];
+        for (const item of data) {            
+            newArr.push ({
+                ...item.node,
+                children: item.children.length ? format(item.children) : []
+            })
+        } 
+        newArr = touchSelcOpts(newArr)
+        return newArr
+    }
+
+    // 过滤一级nav, 当children为空，则认为不应出现
+    function filterLevelOps(){
+        const optArr = format(optsArr)
+        const arr = optArr.filter(ar => ar.children?.length > 0)
+        return arr
+    }
+
+    // 过滤二级掉已有的nav
+    function touchSelcOpts(opts){
+      const tempNavs = [...curNavs]
+      const newOpts = opts.filter(opt => !tempNavs.includes(opt?.uid))
+      return newOpts
+    }
+
+    // tab面板切换
+    function tabChange(key){
+        setcurrentSelectRelatedPageOpt(currentSelectRelatedPageOpt.slice(0, +key))
+        setcurrentKey(key)
+    } 
+
+    // 选择关联页面
+    function selectedHandle(item, step){
+        const {children, linkType, detailType} = item
+        const arr = [...currentSelectRelatedPageOpt, item]
+        setcurrentSelectRelatedPageOpt(arr)
+        setcurrentKey(+step + 1 + '')
+        setdetailType(detailType)
+
+        if (!children.length && linkType === 1) hidePanel()
+        if(callFun) callFun(formatData(arr))
+    }
+
+    // 获取表格数据
+    async function getDataList (config){
+        if(!detailType) return
+        const status = detailType === 6 ? '' : '1'
+        const searchText = config?.searchText || ''
+        const prama = {
+            gongdiStatus: 0,
+            searchText,
+            status,
+            specialStatus: status,
+            searchWord: searchText,
+            activityTitle: searchText,
+            articleDicCode: currentarticleDicCode,
+            pageNum,
+            pageSize: 10,
+        }
+        // 没有文章栏目时先请求一次
+        if (detailType === 4 && !articleDicOpts.length) await getArticleDic();
+        const apiKeys = [siteListApi, designerListApi, caseListApi, articleListApi, specialListApi, activeListApi]
+        const { data, code } = await apiKeys[detailType-1]({...prama, ...config})
+        if(code !== 200 ) return
+        setdetData(data)
+    }
+
+    // 查询文章栏目选项
+    async function getArticleDic(){
+        const res = await articleDicApi({dicModuleCodes: 'DM006'});
+        if(!res?.data) return
+        setarticleDicOpts(res.data)
+        setcurrentarticleDicCode(res.data[0]?.code)
+    }
+
+    // 文章类型切换
+    function radioGroupChange(e){
+        const val = e.target.value
+        setcurrentarticleDicCode(val)
+        getDataList({articleDicCode: val})
+    }
+
+    // 模糊查询  
+    function handleChange(e){
+        const val = e.target.value; 
+        const getList =  _.debounce( () => {
+            getDataList({searchText: val.slice(0, 30)})
+        }, 300)
+        setpageNum(1)
+        getList()
+    }
+
+    // 行点击事件
+    function rowClick(record){
+        record.linkType = 1  // 到这里来的都是末节点了，作个标记
+        const arr = [...currentSelectRelatedPageOpt, record]
+        setcurrentSelectRelatedPageOpt(arr)
+        hidePanel()
+
+        if(callFun) callFun(formatData(arr))
+    }
+
+    // 关闭选择器面板时顺便重置
+    function hidePanel(){
+        setshowSelectPanl(false)
+
+        setcurrentKey('0')
+        setpageNum(1)
+    }
+
+    // 页码变换
+    function pageChange(page){
+        setpageNum(page)
+        getDataList({pageNum: page});
+    }
+
+    return (
+        <>  
+            {showSelectPanl && <div className={styles['card-container']}>
+                <Tabs type="card" tabBarGutter={0} activeKey={currentKey} onChange={tabChange}>
+                    <TabPane tab={currentSelectRelatedPageOpt[0]?.name || '请选择'} key='0'>
+                        {relatedPageOption?.map(item => 
+                            <p style={{cursor: 'pointer'}} key={item.uid} onClick={() => selectedHandle(item, '0')}>{item.name}</p>
+                        )}
+                    </TabPane>
+                    {currentSelectRelatedPageOpt[0]?.children.length && 
+                        <TabPane tab={currentSelectRelatedPageOpt[1]?.name || '请选择'} key='1'>
+                            {currentSelectRelatedPageOpt[0].children.map(item => 
+                                <p style={{cursor: 'pointer'}} key={item.uid} onClick={() => selectedHandle(item, '1')}>{item.name}</p>
+                            )}
+                        </TabPane>}
+                    {(currentSelectRelatedPageOpt[1]?.linkType === 2 || currentSelectRelatedPageOpt[0]?.linkType === 2 ) && 
+                        <TabPane tab='请选择' key= {currentSelectRelatedPageOpt[0]?.linkType === 2 ? '1' : '2'}>
+                            <Search placeholder='可输入关键字进行检索'  onChange={handleChange}/>
+                            {console.log(detailType, articleDicOpts)}
+                                {detailType === 4 && 
+                                    <Radio.Group style={{marginTop: 8}} buttonStyle='solid' size='small' value={currentarticleDicCode} buttonStyle="solid" onChange={radioGroupChange}>
+                                        {articleDicOpts.map(item => <Radio.Button key={item.code} value={item.code}>{item.name}</Radio.Button>)}
+                                    </Radio.Group>
+                                }
+                            <Table
+                                size='small'
+                                style={{marginTop: 8, cursor: 'pointer'}}
+                                columns={ ColumnsObj[`columns_${detailType}`] }
+                                dataSource={detData?.list}
+                                rowKey={(r, i) => i}
+                                onRow={record => {return {onClick: () => rowClick(record)}}}
+                                pagination={{
+                                    current: pageNum,
+                                    pageSize: 10,
+                                    total: detData?.recordTotal,
+                                    onChange: pageChange
+                                }}
+                            />
+                        </TabPane>
+                    }
+                </Tabs>
+            </div>}
+        </>
+    )
+}
